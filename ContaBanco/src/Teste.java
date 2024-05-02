41ca5cec58b0efa5386f7101905b5deb1e215f74
SigmaDePapel <brunoxdz98@gmail.com>
2024-05-02 12:48:30 -0300

    Teste com limpeza de buffer.

---
+++ ContaBanco/src/Teste.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Teste{
+
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.printf("Insira o número da conta: ");
+            int numero = scanner.nextInt();
+
+        scanner.nextLine(); //limpeza do buffer
+
+        System.out.printf("Insira a agência: ");
+            String agencia = scanner.nextLine();
+
+        System.out.printf("Insira seu nome: ");
+            String nomeCliente = scanner.nextLine();
+
+
+        System.out.printf("Insira o saldo: ");
+            float saldo=scanner.nextFloat();
+        scanner.close();
+
+
+        System.out.printf("\"Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque\".");
+
+
+    }
+}
